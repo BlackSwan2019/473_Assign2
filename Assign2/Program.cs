e629ca5cb41d9ded8fe04e36f9340d4216c89d76
Matt Rycraft <mattrycraft@comcast.net>
2019-02-13 15:13:02 -0600

    Update Program.cs

--- Assign2/Program.cs
+++ Assign2/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+/*
+ * Program:     Assignment 2
+ * Author:      Patrick Klesyk, Ben Lane, Matt Rycraft
+ * Z-ID:        Z1782152        Z1806979  Z1818053 
+ * Description: This application is a GUI.
+ *              It is used for a Player/Guild Management
+ *              System, using many of the same features 
+ *              from the console application in Assignment 1
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
